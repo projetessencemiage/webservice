e418b2f9566c1321495804fbdbd43e5651f75538
MarcMiage <m.frogier@gmail.com>
2013-04-08 16:21:38 +0200

    Dev #Test

---
+++ WcfService1/AffichagePrix.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "AffichagePrix" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez AffichagePrix.svc ou AffichagePrix.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class AffichagePrix : IAffichagePrix
+    {
+        public string GetPrixCodePostal(int codePostal)
+        {
+            return "Le prix le moins cher pour le codePostal " + codePostal + " est de 1,42€";
+        }
+    }
+}
---
+++ WcfService1/IAffichagePrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService1
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IAffichagePrix" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IAffichagePrix
+    {
+        [OperationContract]
+        string GetPrixCodePostal(int codePostal);
+    }
+}
